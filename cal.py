a592497368bfd57f19e7995ebebde9b2580dc553
HARIHARAN M <hariharanmct06@gmail.com>
2026-05-07 15:25:01 +0530

    Create cal.py

---
+++ cal.py
@@ -0,0 +1,25 @@
+# Simple Calculator in Python
+
+# Take input from user
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+# Perform calculation
+if operator == "+":
+    print("Result:", num1 + num2)
+
+elif operator == "-":
+    print("Result:", num1 - num2)
+
+elif operator == "*":
+    print("Result:", num1 * num2)
+
+elif operator == "/":
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Error: Division by zero!")
+
+else:
+    print("Invalid operator!")
